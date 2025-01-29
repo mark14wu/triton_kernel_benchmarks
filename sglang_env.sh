b5ef057ff78d94883ca35a8d887e2535c430d8b5
Hao Wu <hwu27@gmu.edu>
2025-01-29 11:25:21 -0500

    add commands and env setup for sglang

---
+++ sglang_env.sh
@@ -0,0 +1,4 @@
+pip install sgl_kernel
+pip install flashinfer -i https://flashinfer.ai/whl/cu124/torch2.4/
+pip install decord
+pip install torchao
---
+++ test_benchmarks/test_sglang/gen_commands.py
@@ -0,0 +1,6 @@
+commands = []
+with open('model_list.txt', 'r') as f:
+    model_list = f.read().splitlines()
+    for model_name in model_list:
+        commands.append(f"python -m sglang.bench_one_batch --model-path {model_name} --load-format dummy")
+open('commands.txt', 'w').write('\n'.join(commands))
