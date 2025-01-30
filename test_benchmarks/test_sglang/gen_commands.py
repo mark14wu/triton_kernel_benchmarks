763a21887ed99b56fb73b919d96749ce2cde6dbb
Hao Wu <hwu27@gmu.edu>
2025-01-30 12:41:17 -0500

    disable CUDAGraph in sglang

--- test_benchmarks/test_sglang/gen_commands.py
+++ test_benchmarks/test_sglang/gen_commands.py
@@ -2,5 +2,5 @@ commands = []
 with open('model_list.txt', 'r') as f:
     model_list = f.read().splitlines()
     for model_name in model_list:
-        commands.append(f"python -m sglang.bench_one_batch --model-path {model_name} --load-format dummy")
+        commands.append(f"python -m sglang.bench_one_batch --model-path {model_name} --load-format dummy --trust-remote-code --disable-cuda-graph")
 open('commands.txt', 'w').write('\n'.join(commands))
